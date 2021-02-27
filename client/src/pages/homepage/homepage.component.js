4ea3a922d7ec6b8b8cdf0bac88151c984e65cc00
VinishkoSensei <fatefulreaper@gmail.com>
2021-02-28 01:17:54 +0300

    finished skeleton, added styles

--- client/src/pages/homepage/homepage.component.js
+++ client/src/pages/homepage/homepage.component.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import './homepage.styles.scss';
+
 import Header from '../../components/header/header.component';
 import Player from '../../components/player/player.component';
 
 const HomePage = () => {
   return (
-    <div>
-      <Header />
-      <Player />
+    <div className="main">
+      <div className="toppart">
+        <Header />
+        <Player />
+      </div>
+      <div className="main-part">
+        <div className="main-space">
+          <div className="main-container">123</div>
+        </div>
+        <div className="rightpanel">
+          <div className="profile"></div>
+          <div className="news"></div>
+        </div>
+      </div>
+      <div className="footer"></div>
     </div>
   );
 };
