a39668f6f53d908e56e5daa5ddff37948136dbd5
VinishkoSensei <fatefulreaper@gmail.com>
2021-02-28 00:12:26 +0300

    added header

---
+++ client/src/components/header/header.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-logo">Logo</div>
+      <div className="header-links">
+        <div>
+          <a href="/">Option 1</a>
+        </div>
+        <div>
+          <a href="/">Option 2</a>
+        </div>
+        <div>
+          <a href="/">Option 3</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
