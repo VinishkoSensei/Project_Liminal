fec2d054f0ba260d2a3e050a6a96989c10e025f0
VinishkoSensei <fatefulreaper@gmail.com>
2021-02-28 02:00:41 +0300

    added dep react-lineto, created test lines, created main 4 blocks

--- client/src/pages/homepage/homepage.component.js
+++ client/src/pages/homepage/homepage.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './homepage.styles.scss';
+import LineTo from 'react-lineto';
 
 import Header from '../../components/header/header.component';
 import Player from '../../components/player/player.component';
@@ -13,7 +14,21 @@ const HomePage = () => {
       </div>
       <div className="main-part">
         <div className="main-space">
-          <div className="main-container">123</div>
+          <div className="main-container">
+            <div className="cards-container">
+              <div className="cards top">
+                <div className="card lt"></div>
+                <div className="card rt"></div>
+              </div>
+              <div className="card-center">
+                <div className="card-center-icon" />
+              </div>
+              <div className="cards bottom">
+                <div className="card lb"></div>
+                <div className="card rb"></div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="rightpanel">
           <div className="profile"></div>
@@ -21,6 +36,10 @@ const HomePage = () => {
         </div>
       </div>
       <div className="footer"></div>
+      <LineTo from="card-center-icon" to="card lt" toAnchor="bottom right" />
+      <LineTo from="card-center-icon" to="card rt" toAnchor="bottom left" />
+      <LineTo from="card-center-icon" to="card lb" toAnchor="top right" />
+      <LineTo from="card-center-icon" to="card rb" toAnchor="top left" />
     </div>
   );
 };
