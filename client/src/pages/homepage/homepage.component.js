92a6b729931c2062feef0057ddcbb30f3bd298be
VinishkoSensei <fatefulreaper@gmail.com>
2021-02-28 00:12:26 +0300

    added header

---
+++ client/src/pages/homepage/homepage.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/header/header.component';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      123body123
+    </div>
+  );
+};
+
+export default HomePage;
