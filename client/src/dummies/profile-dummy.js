5799d3d7a0be0ea7dfc11bcd7a6e2801ecc023f6
VinishkoSensei <fatefulreaper@gmail.com>
2021-02-28 19:29:40 +0300

    added and styled profile-skeleton

---
+++ client/src/dummies/profile-dummy.js
@@ -0,0 +1,10 @@
+export const profiledummy = {
+  id: 0,
+  login: 'johndoe',
+  email: 'johndoe@gmail.com',
+  name: 'John Doe',
+  photourl: 'images/1.jpg',
+  dateofbirth: '01-01-1997',
+  dateofregistration: '28-02-2021',
+  status: 'paid',
+};
