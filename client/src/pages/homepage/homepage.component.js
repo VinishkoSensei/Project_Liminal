e002d5c4f06700adc4d58e3b5ced46bcece7706b
VinishkoSensei <fatefulreaper@gmail.com>
2021-03-01 00:55:51 +0300

    styled main cards

--- client/src/pages/homepage/homepage.component.js
+++ client/src/pages/homepage/homepage.component.js
@@ -26,15 +26,31 @@ const HomePage = () => {
           <div className="main-container">
             <div className="cards-container">
               <div className="cards top">
-                <div className="card lt"></div>
-                <div className="card rt"></div>
+                <div className="card lt">
+                  <div className="card-image-container">
+                    <img src="/images/ai.svg" alt="ai" />
+                  </div>
+                </div>
+                <div className="card rt">
+                  <div className="card-image-container">
+                    <img src="/images/broadcast.svg" alt="broadcast" />
+                  </div>
+                </div>
               </div>
               <div className="card-center">
                 <div className="card-center-icon" />
               </div>
               <div className="cards bottom">
-                <div className="card lb"></div>
-                <div className="card rb"></div>
+                <div className="card lb">
+                  <div className="card-image-container">
+                    <img src="/images/playlist.svg" alt="playlist" />
+                  </div>
+                </div>
+                <div className="card rb">
+                  <div className="card-image-container">
+                    <img src="/images/music.svg" alt="music" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
