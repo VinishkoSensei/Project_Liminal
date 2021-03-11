72f3b7621360a3c214b5f50ce39b64a0e788cbc5
VinishkoSensei <fatefulreaper@gmail.com>
2021-03-12 02:46:20 +0300

    cleaned up a bit

--- client/src/pages/homepage/homepage.component.js
+++ client/src/pages/homepage/homepage.component.js
@@ -1,20 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './homepage.styles.scss';
 
 import Header from '../../components/header/header.component';
 import Player from '../../components/player/player.component';
 
-import { profiledummy } from '../../dummies/profile-dummy';
-import { tracksdummy } from '../../dummies/tracks-dummy';
 import MainMenuLines from '../../components/mainmenu-lines/mainmenu-lines.component';
-
 import ChangingCards from '../../components/changingcards/changingcards.component';
 import MainMenu from '../../components/mainmenu/mainmenu.component';
 import Profile from '../../components/profile/profile.component';
 
 const HomePage = () => {
-  const [changedCards, setChangedCards] = useState(false);
-
   return (
     <div className="main">
       <div className="toppart">
@@ -25,8 +20,8 @@ const HomePage = () => {
         <div className="main-space">
           <div className="main-container">
             <div className="cards-container">
-              {/*<ChangingCards />*/}
-              <MainMenu />
+              <ChangingCards />
+              {/* <MainMenu />*/}
             </div>
           </div>
         </div>
