56230bef26ebb4e833fc56434efd98a2512a069a
VinishkoSensei <fatefulreaper@gmail.com>
2021-03-25 11:31:45 +0300

    trying to make it work

--- server/server.js
+++ server/server.js
@@ -2,10 +2,23 @@ const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const Throttle = require('throttle');
+const { PassThrough } = require('stream');
+const Path = require('path');
+
+const { startStreaming } = require('./streaming.js');
+const { streamHandler } = require('./streaming.js');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+startStreaming();
+app.use(express.static('public'));
+
+app.get('/stream', (req, res) => {
+  streamHandler(req, res);
+});
 
 app.listen(3001, () => {
   console.log('running');
