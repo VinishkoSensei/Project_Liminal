8c163d962147221ce8c91ac213cd967a88932168
VinishkoSensei <fatefulreaper@gmail.com>
2021-03-27 05:45:55 +0300

    added some deps, created test radio, created test music player

---
+++ server/routes/index.js
@@ -0,0 +1,32 @@
+const File = require('inert');
+const { streamHandler, streamTrack } = require('../streaming');
+
+const plugin = {
+  name: 'streamServer',
+
+  register: async (server, options) => {
+    await server.register(File);
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: function (request, h) {
+        return h.file('index.html');
+      },
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/stream',
+      handler: streamHandler,
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/track',
+      handler: streamTrack,
+    });
+  },
+};
+
+module.exports = plugin;
