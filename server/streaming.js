228738be3d0c82e9696fd29697c6e8ad62934722
VinishkoSensei <fatefulreaper@gmail.com>
2021-03-27 05:45:55 +0300

    added some deps, created test radio, created test music player

--- server/streaming.js
+++ server/streaming.js
@@ -2,26 +2,38 @@ const Fs = require('fs');
 const { PassThrough } = require('stream');
 const Throttle = require('throttle');
 
+const sinks = [];
 const songs = [
   Fs.createReadStream('./files/music/1.mp3'),
   Fs.createReadStream('./files/music/2.mp3'),
   Fs.createReadStream('./files/music/3.mp3'),
 ];
 
-const sinks = [];
-
-const throttle = new Throttle((192027 / 10) * 1.3);
-throttle.on('data', (chunk) => {
-  sinks.forEach((sink) => {
-    sink.write(chunk);
-  });
-});
-
-const streamHandler = (req, res) => {
+const streamHandler = (request, h) => {
   const sink = new PassThrough();
   sinks.push(sink);
-  res.set('Content-Type', 'audio/mpeg');
-  res.send(sink);
+  console.log(sinks.length);
+  return h.response(sink).type('audio/mpeg');
+};
+
+const streamTrack = (req, h) => {
+  const path = `./files/music/1.mp3`;
+  const stat = Fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+  if (range) {
+    const parts = range.replace(/bytes=/, '').split('-');
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunksize = end - start + 1;
+    const file = Fs.createReadStream(path, { start, end });
+    const response = h.response(file);
+    response.header('Content-Range', `bytes ${start}-${end}/${fileSize}`);
+    response.header('Accept-Ranges', `bytes`);
+    response.header('Content-Length', `${chunksize}`);
+    response.header('Content-Type', `audio/mpeg`);
+    return response;
+  }
 };
 
 const startStreaming = () => {
@@ -29,10 +41,12 @@ const startStreaming = () => {
 
   (function playLoop() {
     const song = songs[songNum++];
-    console.log(song);
+    const throttle = new Throttle(128000 / 8);
+    throttle.on('data', (chunk) => sinks.forEach((sink) => sink.write(chunk)));
+    if (songs.length === songNum) songNum = 0;
     song.pipe(throttle, { end: false });
     song.on('end', playLoop);
   })();
 };
 
-module.exports = { streamHandler, startStreaming };
+module.exports = { streamHandler, startStreaming, streamTrack };
