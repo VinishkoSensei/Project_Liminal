fd5a5ab87f7e2da81a608009e6f3c3990c2721f9
VinishkoSensei <fatefulreaper@gmail.com>
2021-04-01 05:54:44 +0300

    made track search

--- client/src/components/card-search/card-search.component.js
+++ client/src/components/card-search/card-search.component.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './card-search.styles.scss';
 
 const CardSearch = ({ changedCards }) => {
+  const [tracks, setTracks] = useState();
   const [query, setQuery] = useState('');
   const [searchbarOnTop, setSearchbarOnTop] = useState(false);
+  const [searchType, setSearchType] = useState('all');
   const handleChange = (event) => {
     setQuery(event.target.value);
   };
@@ -11,7 +13,48 @@ const CardSearch = ({ changedCards }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     setSearchbarOnTop(query.length);
+    try {
+      switch (searchType) {
+        case 'authors':
+          const res = await fetch(
+            `http://localhost:3001/gettracksbyauthor/${query}`
+          );
+          const data = await res.json();
+          setTracks(data);
+          break;
+        case 'tracks':
+          const res1 = await fetch(
+            `http://localhost:3001/gettracksbyname/${query}`
+          );
+          const data1 = await res1.json();
+          setTracks(data1);
+          break;
+        default:
+          const res2 = await fetch(
+            `http://localhost:3001/gettracksbynameandauthor/${query}`
+          );
+          const data2 = await res2.json();
+          setTracks(data2);
+          break;
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  /*useEffect(() => {
+    const getTrackList = async () => {
+      try {
+        const res = await fetch(`http://localhost:3001/gettracklist`);
+        const data = await res.json();
+        setTracks(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getTrackList();
+  }, []);*/
 
   return (
     <div className={`card-search${searchbarOnTop ? ' searched' : ''}`}>
@@ -31,6 +74,53 @@ const CardSearch = ({ changedCards }) => {
           <input type="text" onChange={handleChange} />
         </div>
       </form>
+      <div className={`searchlist${searchbarOnTop ? ' searched' : ''}`}>
+        <div className="searchlist-buttons">
+          <div
+            className={`button${searchType === 'all' ? ' selected' : ''}`}
+            onClick={() => setSearchType('all')}
+          >
+            All
+          </div>
+          <div
+            className={`button${searchType === 'authors' ? ' selected' : ''}`}
+            onClick={() => setSearchType('authors')}
+          >
+            Authors
+          </div>
+          <div
+            className={`button${searchType === 'tracks' ? ' selected' : ''}`}
+            onClick={() => setSearchType('tracks')}
+          >
+            Tracks
+          </div>
+        </div>
+        <div className="searchlist-list">
+          <div className="searchlist-tracks">
+            {tracks?.map((track) => (
+              <div className="track" key={track.id}>
+                <div className="track-cover-container">
+                  <img
+                    src={`http://localhost:3001/gettrackcover/${track.cover}`}
+                    alt="cover"
+                    width="60px"
+                    height="60px"
+                  />
+                </div>
+                <div className="track-info">
+                  <div>{track.name}</div>
+                  <div>
+                    <i>{track.author}</i>
+                  </div>
+                </div>
+                <div className="track-duration">
+                  <p>{track.duration}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
       {/*<div className="card-big-table">
         tracksdummy.map((track) => (
           <div className="track" key={track.id}>
