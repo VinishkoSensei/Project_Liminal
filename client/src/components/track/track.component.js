eeb0357f6c7c521f5f7dacd962abf22fe00e45ef
VinishkoSensei <fatefulreaper@gmail.com>
2021-04-02 18:12:08 +0300

    created track component

---
+++ client/src/components/track/track.component.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './track.styles.scss';
+
+const Track = ({ track, index, playTrack, deleteFromPlaylist }) => {
+  return (
+    <div className="track" key={index}>
+      <div className="track-cover-container">
+        <img
+          src={`http://localhost:3001/gettrackcover/${track.cover}`}
+          alt="cover"
+          width="60px"
+          height="60px"
+        />
+      </div>
+      <div className="track-info">
+        <div>{track.name}</div>
+        <div>
+          <i>{track.author}</i>
+        </div>
+      </div>
+      <div className="track-duration">
+        <p>{track.duration}</p>
+      </div>
+      <div className="track-menu">
+        <div
+          className="track-menu-item"
+          onClick={() => playTrack(track.id, index)}
+          style={{ backgroundImage: `url('/images/play.svg')` }}
+        />
+        <div
+          className="track-menu-item"
+          onClick={() => deleteFromPlaylist(index)}
+          style={{ backgroundImage: `url('/images/trash.svg')` }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Track;
