2cb8397ff08a78e26bb44b6a9d5e8ad4fab2b073
VinishkoSensei <fatefulreaper@gmail.com>
2021-04-05 03:07:10 +0300

    removed downloading empty profile image when page loads

--- client/src/components/profile/profile.component.js
+++ client/src/components/profile/profile.component.js
@@ -3,19 +3,8 @@ import './profile.styles.scss';
 
 const Profile = () => {
   const [minifiedProfile, setMinifiedProfile] = useState(false);
-  const [profile, setProfile] = useState({
-    id: -1,
-    first_name: '',
-    last_name: '',
-    middle_name: '',
-    phone: '',
-    email: '',
-    avatar: '',
-    birth_date: '',
-    subscribed: false,
-  });
+  const [profile, setProfile] = useState();
 
-  //console.log(profile);
   useEffect(() => {
     const getProfileInfo = async () => {
       const res = await fetch(`http://localhost:3001/getprofile`);
@@ -27,31 +16,35 @@ const Profile = () => {
   }, []);
 
   return (
-    <div
-      className={`profile${minifiedProfile ? ' minified' : ''}`}
-      onClick={() => setMinifiedProfile(!minifiedProfile)}
-    >
-      <div className="profile-icon-container">
-        <img
-          src={`http://localhost:3001/getprofileimage/${profile.avatar}`}
-          alt="profileimage"
-        />
-        <div className="profile-info-minified">
-          {profile.first_name}&nbsp;{profile.last_name}
+    <div>
+      {profile ? (
+        <div
+          className={`profile${minifiedProfile ? ' minified' : ''}`}
+          onClick={() => setMinifiedProfile(!minifiedProfile)}
+        >
+          <div className="profile-icon-container">
+            <img
+              src={`http://localhost:3001/getprofileimage/${profile.avatar}`}
+              alt="profileimage"
+            />
+            <div className="profile-info-minified">
+              {profile.first_name}&nbsp;{profile.last_name}
+            </div>
+          </div>
+          <div className="profile-info">
+            <div className="profile-info-minifying">
+              {profile.first_name}&nbsp;{profile.last_name}
+            </div>
+            <div className="profile-info-minifying">
+              <i>{profile.email}</i>
+            </div>
+            <div className="profile-info-minifying">{profile.birth_date}</div>
+            <div className="profile-info-minifying">
+              {profile.subscribed ? 'subscribed' : 'not subscribed'}
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="profile-info">
-        <div className="profile-info-minifying">
-          {profile.first_name}&nbsp;{profile.last_name}
-        </div>
-        <div className="profile-info-minifying">
-          <i>{profile.email}</i>
-        </div>
-        <div className="profile-info-minifying">{profile.birth_date}</div>
-        <div className="profile-info-minifying">
-          {profile.subscribed ? 'subscribed' : 'not subscribed'}
-        </div>
-      </div>
+      ) : null}
     </div>
   );
 };
