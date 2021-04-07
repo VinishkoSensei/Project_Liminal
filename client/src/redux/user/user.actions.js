d04c5fc29957b5e485ab4485b4b395e38f48d129
VinishkoSensei <fatefulreaper@gmail.com>
2021-04-08 02:19:36 +0300

    added user to redux

---
+++ client/src/redux/user/user.actions.js
@@ -0,0 +1,16 @@
+import ProfileActionTypes from './user.types';
+
+export const signInStart = ({ email, password }) => ({
+  type: ProfileActionTypes.SIGN_IN_START,
+  payload: { email, password },
+});
+
+export const signInSuccess = (user) => ({
+  type: ProfileActionTypes.SIGN_IN_SUCCESS,
+  payload: user,
+});
+
+export const signInFailure = (e) => ({
+  type: ProfileActionTypes.SIGN_IN_FAILURE,
+  payload: e,
+});
