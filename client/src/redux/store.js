6a8a25272f091e546be206836b6c4173ae9b1257
VinishkoSensei <fatefulreaper@gmail.com>
2021-04-08 02:19:36 +0300

    added user to redux

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,14 +2,14 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import { persistStore } from 'redux-persist';
-//import createSagaMiddleware from 'redux-saga';
-//import rootSaga from './root-saga';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './root-saga';
 
-//const sagaMiddleware = createSagaMiddleware();
-const middlewares = [logger]; //sagaMiddleware];
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [logger, sagaMiddleware];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-//sagaMiddleware.run(rootSaga);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
