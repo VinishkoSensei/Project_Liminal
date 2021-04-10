9148804eb5f3fcbd1c558741b124f5d898a4e296
VinishkoSensei <fatefulreaper@gmail.com>
2021-04-11 02:19:54 +0300

    changed profile and css

--- client/src/components/profile/profile.component.js
+++ client/src/components/profile/profile.component.js
@@ -13,30 +13,30 @@ const Profile = ({ signInStart, profile }) => {
   return (
     <div>
       {profile ? (
-        <div
-          className={`profile${minifiedProfile ? ' minified' : ''}`}
-          onClick={() => setMinifiedProfile(!minifiedProfile)}
-        >
-          <div className="profile-icon-container">
-            <img
-              src={`http://localhost:3001/getprofileimage/${profile.avatar}`}
-              alt="profileimage"
-            />
-            <div className="profile-info-minified">
+        <div>
+          <div
+            className={`profile-main`}
+            onClick={() => setMinifiedProfile(!minifiedProfile)}
+          >
+            <div className="profile-icon-container">
+              <img
+                src={`http://localhost:3001/getprofileimage/${profile.avatar}`}
+                alt="profileimage"
+              />
+            </div>
+            <div className="profile-info-main">
               {profile.first_name}&nbsp;{profile.last_name}
             </div>
           </div>
-          <div className="profile-info">
-            <div className="profile-info-minifying">
-              {profile.first_name}&nbsp;{profile.last_name}
-            </div>
-            <div className="profile-info-minifying">
+          <div
+            className={`profile-info${minifiedProfile ? ' minified' : ''}`}
+            onClick={() => setMinifiedProfile(!minifiedProfile)}
+          >
+            <div>
               <i>{profile.email}</i>
             </div>
-            <div className="profile-info-minifying">{profile.birth_date}</div>
-            <div className="profile-info-minifying">
-              {profile.subscribed ? 'subscribed' : 'not subscribed'}
-            </div>
+            <div>{profile.birth_date}</div>
+            <div>{profile.subscribed ? 'subscribed' : 'not subscribed'}</div>
           </div>
         </div>
       ) : null}
