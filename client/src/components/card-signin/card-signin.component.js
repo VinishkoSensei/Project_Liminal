2e16488e11565401603c180c749fc52691517b51
VinishkoSensei <fatefulreaper@gmail.com>
2021-04-17 03:58:16 +0300

    added custombutton and forminput

--- client/src/components/card-signin/card-signin.component.js
+++ client/src/components/card-signin/card-signin.component.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CustomButton from '../custombutton/custombutton.component';
+import FormInput from '../forminput/forminput.component';
 import './card-signin.styles.scss';
 import { connect } from 'react-redux';
 
-const CardSignIn = ({changedCards}) => {
+const CardSignIn = ({ changedCards }) => {
+  const selectedItemInitialState = {
+    email: '',
+    password: '',
+  };
+
+  const [selectedItem, setSelectedItem] = useState(selectedItemInitialState);
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+    setSelectedItem({ ...selectedItem, [name]: value });
+  };
+  const { email, password } = selectedItem;
+  const Inputs = [
+    {
+      name: 'email',
+      value: email,
+      label: 'Email:',
+      type: 'email',
+    },
+    {
+      name: 'password',
+      value: password,
+      label: 'Password:',
+      type: 'password',
+    },
+  ];
+
   return (
     <div className="card-signin">
-      <form>
-        <input placeholder="Email"/>
-        <input placeholder="Password" type="password"/>
-        <input type="submit" value="Sign In"/>
-        </form>
+      <form method="post">
+        {Inputs.map((input) => (
+          <FormInput
+            name={input.name}
+            value={input.value}
+            label={input.label}
+            handleChange={handleChange}
+            type={input.type}
+            required
+          />
+        ))}
+        <CustomButton type="submit">Sign In</CustomButton>
+      </form>
       <div className={`card-big-blur${changedCards ? '' : ' disabled'}`}></div>
-      </div>
+    </div>
   );
 };
 /*
