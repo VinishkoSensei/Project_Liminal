49a4e9d3ec1fd250cf7d12d758e20650d1b8ce9f
VinishkoSensei <fatefulreaper@gmail.com>
2021-04-19 03:43:40 +0300

    created signup

--- client/src/redux/user/user.actions.js
+++ client/src/redux/user/user.actions.js
@@ -14,3 +14,25 @@ export const signInFailure = (e) => ({
   type: ProfileActionTypes.SIGN_IN_FAILURE,
   payload: e,
 });
+
+export const signUpStart = (
+  email,
+  firstname,
+  lastname,
+  date,
+  phone,
+  password
+) => ({
+  type: ProfileActionTypes.SIGN_UP_START,
+  payload: { email, firstname, lastname, date, phone, password },
+});
+
+export const signUpSuccess = ({ user, additionalData }) => ({
+  type: ProfileActionTypes.SIGN_UP_SUCCESS,
+  payload: { user, additionalData },
+});
+
+export const signUpFailure = (error) => ({
+  type: ProfileActionTypes.SIGN_UP_FAILURE,
+  payload: error,
+});
