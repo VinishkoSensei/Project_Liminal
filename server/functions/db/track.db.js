58cefd706adbb14e0bb65dfe8210e56f3107c1d9
VinishkoSensei <fatefulreaper@gmail.com>
2021-04-27 02:47:42 +0300

    now backend code uses db functions instead of sql queries, fixed bugs

--- server/functions/db/track.db.js
+++ server/functions/db/track.db.js
@@ -1,49 +1,35 @@
 const getTrackList = async (req, h) => {
   const db = req.getDb();
-  const trackData = await db.any(
-    `SELECT t.id, t."path", t."name", t.cover, to_char(t.duration,'MI:SS') as "duration", a.nickname as "author", g.name as "genre" FROM liminal.track t JOIN liminal.author a ON t.author_id=a.id JOIN liminal.genre g ON t.genre_id=g.id order by t.id`
-  );
+  const trackData = await db.func(`liminal.gettracklist`);
   return h.response(trackData);
 };
 
 const getTracksByNameAndAuthor = async (req, h) => {
   const db = req.getDb();
   const nm = req.params.name;
-  const trackData = await db.any(
-    `SELECT t.id, t."path", t."name", t.cover, to_char(t.duration,'MI:SS') as "duration", a.nickname as "author", g.name as "genre" FROM liminal.track t JOIN liminal.author a ON t.author_id=a.id JOIN liminal.genre g ON t.genre_id=g.id WHERE lower(t."name") like '%'||lower($1)||'%' OR lower(a.nickname) like '%'||lower($1)||'%' order by t.id`,
-    nm
-  );
+  const trackData = await db.func(`liminal.gettracklist`, [nm, nm]);
   return h.response(trackData);
 };
 
 const getTracksByName = async (req, h) => {
   const db = req.getDb();
   const nm = req.params.name;
-  const trackData = await db.any(
-    `SELECT t.id, t."path", t."name", t.cover, to_char(t.duration,'MI:SS') as "duration", a.nickname as "author", g.name as "genre" FROM liminal.track t JOIN liminal.author a ON t.author_id=a.id JOIN liminal.genre g ON t.genre_id=g.id WHERE lower(t."name") like '%'||lower($1)||'%' order by t.id`,
-    nm
-  );
+  const trackData = await db.func(`liminal.gettracklist`, [nm]);
   return h.response(trackData);
 };
 
 const getTracksByAuthor = async (req, h) => {
   const db = req.getDb();
   const nm = req.params.name;
-  const trackData = await db.any(
-    `SELECT t.id, t."path", t."name", t.cover, to_char(t.duration,'MI:SS') as "duration", a.nickname as "author", g.name as "genre" FROM liminal.track t JOIN liminal.author a ON t.author_id=a.id JOIN liminal.genre g ON t.genre_id=g.id WHERE lower(a.nickname) like '%'||lower($1)||'%' order by t.id`,
-    nm
-  );
+  const trackData = await db.func(`liminal.gettracklist`, [null, nm]);
   return h.response(trackData);
 };
 
-const getTrack = async (trackId) => {
+const getTrack = async (req, h) => {
+  const trackId = req.params.trackId;
   const db = req.getDb();
-  const track = await db
-    .one('SELECT * FROM liminal.track WHERE id = $1', trackId)
-    .then((trackData) => {
-      return trackData;
-    });
-  return track;
+  const track = await db.func('liminal.gettrack', trackId);
+  return track[0];
 };
 
 module.exports = {
