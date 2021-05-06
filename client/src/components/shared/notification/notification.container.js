b54496e689c95d0a614f22428eaff65503bb122b
VinishkoSensei <fatefulreaper@gmail.com>
2021-05-07 02:50:16 +0300

    deleted test button and refs

--- client/src/components/shared/notification/notification.container.js
+++ client/src/components/shared/notification/notification.container.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './notification.styles.scss';
-import _ from 'lodash';
 import Notification from './notification.component';
-import { addNotificationStart } from '../../../redux/notification/notification.actions';
-import { notificationTypes } from './notification.types';
 
-const NotificationContainer = ({ notificationArray, addNotificationStart }) => {
+const NotificationContainer = ({ notificationArray }) => {
   return (
     <div className="notification-container">
       {notificationArray.map((notification) => (
         <Notification notification={notification} />
       ))}
-      <button
-        onClick={() =>
-          addNotificationStart(`Bruh${_.uniqueId()}`, notificationTypes.ERROR)
-        }
-      >
-        ADD
-      </button>
     </div>
   );
 };
@@ -27,12 +17,4 @@ const mapStateToProps = (state) => ({
   notificationArray: state.notification.notificationArray,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  addNotificationStart: (text, type) =>
-    dispatch(addNotificationStart(text, type)),
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(NotificationContainer);
+export default connect(mapStateToProps)(NotificationContainer);
