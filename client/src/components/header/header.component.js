a8e17782934530e0e90dc5794277c70d667e4796
VinishkoSensei <fatefulreaper@gmail.com>
2021-05-10 22:06:07 +0300

    added logo and favicon, changed css

--- client/src/components/header/header.component.js
+++ client/src/components/header/header.component.js
@@ -5,7 +5,9 @@ import LangSelector from '../../components/lang-selector/lang-selector.component
 const Header = () => {
   return (
     <div className="header">
-      <div className="header-logo">Logo</div>
+      <div className="header-logo">
+        <img src="/images/logo.svg" alt="Logo" />
+      </div>
       <div className="header-links">
         <LangSelector />
       </div>
