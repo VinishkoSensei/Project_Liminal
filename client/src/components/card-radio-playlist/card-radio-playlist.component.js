ec2a4af2fddafd68f66e8d856cce2e13cdbbd5df
VinishkoSensei <fatefulreaper@gmail.com>
2021-05-20 23:22:02 +0300

    moved card radio playlist to another component

---
+++ client/src/components/card-radio-playlist/card-radio-playlist.component.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './card-radio-playlist.styles.scss';
+import Track from '../track/track.component';
+
+const CardRadioPlaylist = ({ radioQueue, setRadioQueue }) => {
+  const deleteFromRadioQueue = async (index) => {
+    const response = await fetch(
+      `http://localhost:3001/deletetrackfromradioqueue`,
+      {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          index: index,
+        }),
+      }
+    );
+    const newRadioQueue = await response.json();
+    setRadioQueue(newRadioQueue);
+  };
+
+  return (
+    <div className="card-radio-playlist">
+      <div className="card-big-table">
+        {radioQueue?.map((track, index) => (
+          <Track
+            track={track}
+            index={index}
+            key={index}
+            deleteFromRadioQueue={() => deleteFromRadioQueue(track.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardRadioPlaylist;
