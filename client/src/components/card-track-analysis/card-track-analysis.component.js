0266e0aad587ad7ac01971913506e61dc0121a86
VinishkoSensei <fatefulreaper@gmail.com>
2021-05-28 17:02:33 +0300

    got rid of getElementByIds

--- client/src/components/card-track-analysis/card-track-analysis.component.js
+++ client/src/components/card-track-analysis/card-track-analysis.component.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import FormFileInput from '../forminputs/formfileinput/formfileinput.component';
 import './card-track-analysis.styles.scss';
 import * as mm from 'music-metadata-browser';
 
-const CardTrackAnalysis = ({ changedCards }) => {
+const CardTrackAnalysis = () => {
+  const [suggestedPoints, setSuggestedPoints] = useState([]);
+  const canvasRef = useRef(null);
+
   const getAverage = (list) => {
     return list ? list.reduce((prev, curr) => prev + curr, 0) / list.length : 0;
   };
@@ -39,18 +43,17 @@ const CardTrackAnalysis = ({ changedCards }) => {
     return analyser;
   };
 
-  const handleTrackAnalysis = async (e) => {
-    const resv = [];
+  const fileSelectAndAnalyseHandler = (e) => {
+    const file = e.target.files[0];
     var type = '';
     const fr = new FileReader();
-    fr.readAsDataURL(document.getElementById('audio_file').files[0]);
+    fr.readAsDataURL(file);
     fr.onloadend = (e) => {
-      mm.parseBlob(document.getElementById('audio_file').files[0]).then(
-        (metadata) => {
-          const audio = createAudio(e.target.result, 0, 4.0);
-          audio.play();
-          audio.ontimeupdate = (e) => {
-            /*if (e.target.currentTime > 44.416417 - 10 && !alreadyWorking) {
+      mm.parseBlob(file).then((metadata) => {
+        const audio = createAudio(e.target.result, 0, 4.0);
+        audio.play();
+        audio.ontimeupdate = (e) => {
+          /*if (e.target.currentTime > 44.416417 - 10 && !alreadyWorking) {
           alreadyWorking = true;
           var fadeout = setInterval(function () {
             if (audio.volume > 0.35) {
@@ -64,136 +67,132 @@ const CardTrackAnalysis = ({ changedCards }) => {
         if (e.target.currentTime >= 44.241417) {
           audio.volume = 1.0;
         }*/
-          };
-          audio.onended = (e) => {
-            console.log(resv);
-          };
+        };
+        /*audio.onended = (e) => {
+          console.log(suggestedPoints);
+        };*/
 
-          const canvas = document.getElementById('canvas');
-          const canvasCtx = canvas.getContext('2d');
-          const analyser = createAudioContextAndGetAnalyser(audio);
-          const bufferLength = analyser.frequencyBinCount;
-          const barWidth = canvas.width / 255;
+        const canvas = canvasRef.current;
+        const canvasCtx = canvas.getContext('2d');
+        const analyser = createAudioContextAndGetAnalyser(audio);
+        const bufferLength = analyser.frequencyBinCount;
+        const barWidth = canvas.width / 255;
 
-          let dataArray = new Uint8Array(bufferLength);
-          let maybePeak = null;
-          let num = 0;
-          let nummin = 0;
-          let summin = [];
-          let summax = [];
+        let dataArray = new Uint8Array(bufferLength);
+        let maybePeak = null;
+        let num = 0;
+        let nummin = 0;
+        let summin = [];
+        let summax = [];
 
-          const draw = () => {
-            clearCanvas(canvasCtx, canvas);
-            if (!audio.paused) requestAnimationFrame(draw, canvas);
+        const draw = () => {
+          clearCanvas(canvasCtx, canvas);
+          if (!audio.paused) requestAnimationFrame(draw, canvas);
 
-            let x = 0;
+          let x = 0;
 
-            analyser.getByteFrequencyData(dataArray);
-            const sum = dataArray
-              .slice(416, 419)
-              .reduce((acc, elem) => acc + elem, 0);
+          analyser.getByteFrequencyData(dataArray);
+          const sum = dataArray
+            .slice(416, 419)
+            .reduce((acc, elem) => acc + elem, 0);
 
-            dataArray.forEach((elem, index) => {
-              if (index === 0) {
-                if (elem < 120) {
-                  type = 'low';
-                  num = 0;
-                  nummin++;
-                  summin.push(sum);
-                  maybePeak = null;
-                }
+          dataArray.forEach((elem, index) => {
+            if (index === 0) {
+              if (elem < 120) {
+                type = 'low';
+                num = 0;
+                nummin++;
+                summin.push(sum);
+                maybePeak = null;
+              }
 
-                if (elem > 240 && type === 'low' && audio.currentTime > 15) {
-                  num++;
-                  summax.push(sum);
-                  type = 'high';
-                  maybePeak = audio.currentTime;
-                } else {
-                  if (audio.currentTime > maybePeak && maybePeak) {
-                    if (elem > 240) num++;
-                    if (num === 200) {
-                      if (
-                        nummin >= 200 &&
-                        getAverage(summax) / getAverage(summin) > 20
-                      ) {
-                        console.log('Pushing', maybePeak);
-                        resv.push(maybePeak);
-                      }
-                      summin = [];
-                      summax = [];
-                      nummin = 0;
+              if (elem > 240 && type === 'low' && audio.currentTime > 15) {
+                num++;
+                summax.push(sum);
+                type = 'high';
+                maybePeak = audio.currentTime;
+              } else {
+                if (audio.currentTime > maybePeak && maybePeak) {
+                  if (elem > 240) num++;
+                  if (num === 200) {
+                    if (
+                      nummin >= 200 &&
+                      getAverage(summax) / getAverage(summin) > 20
+                    ) {
+                      console.log('Pushing', maybePeak);
+                      setSuggestedPoints([...suggestedPoints, maybePeak]);
                     }
+                    summin = [];
+                    summax = [];
+                    nummin = 0;
                   }
                 }
               }
-              const barHeight = elem * 2;
-              fillCanvas({
-                canvasCtx: canvasCtx,
-                color: {
-                  red: barHeight / 3 + 100,
-                  green: 50,
-                  blue: 255 - barHeight,
-                },
-                rectangle: {
-                  x: x,
-                  y: canvas.height - barHeight,
-                  width: barWidth,
-                  height: barHeight,
-                },
-              });
-              x += barWidth + 1;
-            });
-
+            }
+            const barHeight = elem * 2;
             fillCanvas({
               canvasCtx: canvasCtx,
               color: {
-                red: 0,
-                green: 0,
-                blue: 255,
+                red: barHeight / 3 + 100,
+                green: 50,
+                blue: 255 - barHeight,
               },
               rectangle: {
-                x: 0,
-                y: 0,
-                width:
-                  (canvas.width * audio.currentTime) / metadata.format.duration,
-                height: 20,
+                x: x,
+                y: canvas.height - barHeight,
+                width: barWidth,
+                height: barHeight,
               },
             });
+            x += barWidth + 1;
+          });
 
-            fillCanvas({
-              canvasCtx: canvasCtx,
-              color: {
-                red: 100,
-                green: 200,
-                blue: 200,
-              },
-              rectangle: {
-                x: 0,
-                y: 20,
-                width: canvas.width,
-                height: (canvas.height * sum) / 5 / 255,
-              },
-            });
-            return false;
-          };
+          fillCanvas({
+            canvasCtx: canvasCtx,
+            color: {
+              red: 0,
+              green: 0,
+              blue: 255,
+            },
+            rectangle: {
+              x: 0,
+              y: 0,
+              width:
+                (canvas.width * audio.currentTime) / metadata.format.duration,
+              height: 20,
+            },
+          });
 
-          draw();
-        }
-      );
+          fillCanvas({
+            canvasCtx: canvasCtx,
+            color: {
+              red: 100,
+              green: 200,
+              blue: 200,
+            },
+            rectangle: {
+              x: 0,
+              y: 20,
+              width: canvas.width,
+              height: (canvas.height * sum) / 5 / 255,
+            },
+          });
+          return false;
+        };
+
+        draw();
+      });
     };
   };
 
   return (
     <div className="card-track-analysis">
-      <input
-        type="file"
-        name="audio_file"
-        id="audio_file"
-        onChange={handleTrackAnalysis}
-      ></input>
-      <canvas id="canvas" width="1200" height="600"></canvas>
-
-      <div className={`card-big-blur${changedCards ? ' disabled' : ''}`} />
+      {/*<input type="file" name="audio_file" id="audio_file"></input>*/}
+      <FormFileInput handleChange={fileSelectAndAnalyseHandler} />
+      <canvas id="canvas" ref={canvasRef} width="1200" height="600"></canvas>
+      {suggestedPoints.map((point, index) => (
+        <div key={index}>{point}</div>
+      ))}
     </div>
   );
 };
