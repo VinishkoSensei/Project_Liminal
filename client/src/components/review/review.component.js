6d6b92ef57afc618b135f677688d00a366df5855
VinishkoSensei <fatefulreaper@gmail.com>
2021-06-03 02:10:37 +0300

    finished review panel

--- client/src/components/review/review.component.js
+++ client/src/components/review/review.component.js
@@ -1,14 +1,36 @@
 import React, { useState } from 'react';
 import './review.styles.scss';
+import { connect } from 'react-redux';
+import { addNotificationStart } from '../../redux/notification/notification.actions';
 import CustomButton from '../custombutton/custombutton.component';
 import { Trans } from '@lingui/macro';
 
-const Review = () => {
+const Review = ({ profileid, addNotificationStart }) => {
   const [minifiedProfile, setMinifiedProfile] = useState(true);
-  const [reviewText, setReviewText] = useState('');
+  const reviewInitialState = { theme: '', text: '' };
+  const [review, setReview] = useState(reviewInitialState);
 
   const handleChange = (event) => {
-    setReviewText(event.target.value);
+    const { name, value } = event.target;
+    setReview({ ...review, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch(`http://localhost:3001/createreview`, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        theme: review.theme,
+        reviewtext: review.text,
+        userid: profileid,
+      }),
+    });
+    if (response.ok) {
+      addNotificationStart('Your reply was sended', 'success');
+      setMinifiedProfile(true);
+      setReview(reviewInitialState);
+    }
   };
 
   return (
@@ -20,15 +42,35 @@ const Review = () => {
         >
           <div className="review-container-main">Leave a reply</div>
         </div>
-        <div className={`review-info${minifiedProfile ? ' minified' : ''}`}>
-          <textarea value={reviewText} onChange={handleChange} />
-          <CustomButton type="button">
+        <form className={`review-info${minifiedProfile ? ' minified' : ''}`}>
+          <input
+            placeholder="Theme of the review..."
+            name="theme"
+            value={review.theme}
+            onChange={handleChange}
+          />
+          <textarea
+            placeholder="Review text..."
+            value={review.text}
+            name="text"
+            onChange={handleChange}
+          />
+          <CustomButton type="submit" onClick={handleSubmit}>
             <Trans>Send</Trans>
           </CustomButton>
-        </div>
+        </form>
       </div>
     </div>
   );
 };
 
-export default Review;
+const mapStateToProps = (state) => ({
+  profileid: state.user.profile.id,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  addNotificationStart: (text, notificationType) =>
+    dispatch(addNotificationStart(text, notificationType)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Review);
