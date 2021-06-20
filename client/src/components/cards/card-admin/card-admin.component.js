198fd6e1e19d2cdc5a0929743157187395ed315a
VinishkoSensei <fatefulreaper@gmail.com>
2021-06-21 01:04:47 +0300

    changed file tree, changed relative naming to absolute naming

---
+++ client/src/components/cards/card-admin/card-admin.component.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import './card-admin.styles.scss';
+import CardSearch from 'components/cards/card-search/card-search.component';
+import CardAddTrack from 'components/cards/card-addtrack/card-addtrack.component';
+import CardRadioPlaylist from 'components/cards/card-radio-playlist/card-radio-playlist.component';
+import CardUserList from 'components/cards/card-userlist/card-userlist.component';
+
+const CardAdmin = ({
+  selectedAdminItem,
+  setSelectedAdminItem,
+  isOpened,
+  setIsOpened,
+}) => {
+  const [radioQueue, setRadioQueue] = useState([]);
+
+  useEffect(() => {
+    const getRadioQueue = async () => {
+      const response = await fetch(`http://localhost:3001/getradioqueue`, {
+        method: 'get',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const newRadioQueue = await response.json();
+      setRadioQueue(newRadioQueue);
+    };
+
+    getRadioQueue();
+  }, []);
+
+  const addToRadioQueueStart = ({ track }) => {
+    setRadioQueue([track, ...radioQueue]);
+  };
+  const addToRadioQueueEnd = async ({ track }) => {
+    const response = await fetch(`http://localhost:3001/addtracktoradioqueue`, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        trackid: track.id,
+      }),
+    });
+    const newRadioQueue = await response.json();
+    setRadioQueue(newRadioQueue);
+  };
+
+  const closeAdminCard = () => {
+    setIsOpened(false);
+    setTimeout(() => {
+      if (!isOpened) setSelectedAdminItem(null);
+    }, 2000);
+  };
+
+  const chooseCards = (item) => {
+    switch (item) {
+      case 'track':
+        return (
+          <div className="card-admin-main">
+            <CardSearch noMenu />
+            <CardAddTrack />
+          </div>
+        );
+      case 'users':
+        return (
+          <div className="card-admin-main">
+            <CardUserList />
+          </div>
+        );
+      case 'playlist':
+        return (
+          <div className="card-admin-main">
+            <CardSearch
+              addToRadioQueueStart={addToRadioQueueStart}
+              addToRadioQueueEnd={addToRadioQueueEnd}
+            />
+            <CardRadioPlaylist
+              radioQueue={radioQueue}
+              setRadioQueue={setRadioQueue}
+            />
+          </div>
+        );
+      default:
+        return <div className="card-admin-main"></div>;
+    }
+  };
+
+  return (
+    <div className={`card-admin${isOpened ? ' expanded' : ''}`}>
+      <div className="admin-btn-back" onClick={closeAdminCard} />
+      {chooseCards(selectedAdminItem)}
+    </div>
+  );
+};
+
+export default CardAdmin;
---
+++ client/src/components/cards/card-ai/card-ai.component.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Switch from 'components/shared/switch/switch.component';
+import './card-ai.styles.scss';
+import { Trans } from '@lingui/macro';
+
+const CardAi = () => {
+  const [checked, setChecked] = useState(false);
+  const handleCheckboxChange = () => {
+    setChecked(!checked);
+  };
+
+  return (
+    <div className="card-ai">
+      <div className="card-ai-switch">
+        <span>
+          <Trans>Smart Volume Contol:</Trans>
+        </span>
+        <Switch
+          index="swich-ai"
+          checked={checked}
+          handleCheckboxChange={handleCheckboxChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CardAi;
